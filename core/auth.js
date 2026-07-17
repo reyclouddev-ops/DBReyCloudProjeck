446a68266ec448e8eb179be72d6b6d9a3fdf7510
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-17 23:59:37 +0700

    Update auth.js

--- core/auth.js
+++ core/auth.js
@@ -4,6 +4,10 @@ require("../config.json");
 
 function checkKey(key){
 
+console.log("KEY DARI REQUEST:", key);
+console.log("KEY DARI CONFIG:", config.apiKey);
+
+
 if(key !== config.apiKey){
 
 throw Error(
