fa755aaca7f9068f7f2dcab6225993b35d438477
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-18 22:30:27 +0700

    Update collection.js

--- core/collection.js
+++ core/collection.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 const path = require("path");
 
-class Collection{
+class Collection {
 
-    constructor(databasePath,name){
+    constructor(databasePath, name){
 
         this.name = name;
 
@@ -23,120 +23,125 @@ class Collection{
 
     }
 
+
     read(){
 
         return JSON.parse(
             fs.readFileSync(
-                this.file
+                this.file,
+                "utf8"
             )
         );
 
     }
+
 
     write(data){
 
- insertOne(data){
-
-    const Document =
-    require("./document");
-
-    const docs =
-    this.read();
-
-    const doc =
-    Document.create(data);
-
-    docs.push(doc);
-
-    this.write(docs);
-
-    return doc;
-
-}
-
-        find(){
-
-    return this.read();
-
-        }
-
-        findOne(id){
-
-    return this
-    .read()
-    .find(x=>x._id===id);
-
-        }
-
-        deleteOne(id){
-
-    const docs =
-    this.read();
-
-    const index =
-    docs.findIndex(
-        x=>x._id===id
-    );
-
-    if(index==-1)
-        return false;
-
-    docs.splice(index,1);
-
-    this.write(docs);
-
-    return true;
-
-        }
-
-        updateOne(id,data){
-
-    const docs =
-    this.read();
-
-    const index =
-    docs.findIndex(
-        x=>x._id===id
-    );
-
-    if(index==-1)
-        return null;
-
-    docs[index]={
-
-        ...docs[index],
-
-        ...data,
-
-        updatedAt:
-        new Date()
-
-    };
-
-    this.write(docs);
-
-    return docs[index];
-
-        }
-        
         fs.writeFileSync(
-
             this.file,
-
             JSON.stringify(
-
                 data,
-
                 null,
-
                 2
-
             )
-
         );
 
     }
 
+
+    insertOne(data){
+
+        const Document =
+        require("./document");
+
+        const docs =
+        this.read();
+
+        const doc =
+        Document.create(data);
+
+        docs.push(doc);
+
+        this.write(docs);
+
+        return doc;
+
+    }
+
+
+    find(){
+
+        return this.read();
+
+    }
+
+
+    findOne(id){
+
+        return this
+        .read()
+        .find(x => x._id === id);
+
+    }
+
+
+    deleteOne(id){
+
+        const docs =
+        this.read();
+
+        const index =
+        docs.findIndex(
+            x => x._id === id
+        );
+
+        if(index === -1)
+            return false;
+
+
+        docs.splice(index,1);
+
+        this.write(docs);
+
+        return true;
+
+    }
+
+
+    updateOne(id,data){
+
+        const docs =
+        this.read();
+
+        const index =
+        docs.findIndex(
+            x => x._id === id
+        );
+
+        if(index === -1)
+            return null;
+
+
+        docs[index] = {
+
+            ...docs[index],
+
+            ...data,
+
+            updatedAt:
+            new Date()
+
+        };
+
+
+        this.write(docs);
+
+        return docs[index];
+
+    }
+
 }
+
 
 module.exports = Collection;
