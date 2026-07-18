25e786ce2b6b03cfa89f73ca4fa169e9e4e7d6bc
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-18 21:30:30 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,9 @@ const router = express.Router();
 const database = require("./database");
 const collection = require("./collection");
 const document = require("./document");
+const auth = require("./auth");
 
+router.use("/auth", auth);
 router.use("/database", database);
 router.use("/collection", collection);
 router.use("/document", document);
