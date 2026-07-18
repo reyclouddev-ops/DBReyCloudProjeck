c7bc777247c01c223be66c17570d0cb6b3860cf9
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-18 21:49:32 +0700

    Update register.js

--- public/js/register.js
+++ public/js/register.js
@@ -1,9 +1,41 @@
-document
-.getElementById("registerBtn")
-.onclick=()=>{
+document.getElementById("registerBtn").onclick = async () => {
 
-alert(
-"Register API Coming Soon"
-);
+    const username =
+        document.getElementById("username").value.trim();
 
-}
+    const password =
+        document.getElementById("password").value.trim();
+
+    const confirm =
+        document.getElementById("confirm").value.trim();
+
+    if (password !== confirm) {
+
+        alert("Password tidak sama");
+
+        return;
+
+    }
+
+    const data = await API.request(
+        "/auth/register",
+        "POST",
+        {
+            username,
+            password
+        }
+    );
+
+    if (!data.success) {
+
+        alert("Register gagal");
+
+        return;
+
+    }
+
+    alert("Register berhasil");
+
+    location.href = "/login.html";
+
+};
