f2bc02e7894dcf61635d4208ab1b505116a70ce9
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-18 21:51:05 +0700

    Update dashboard.js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,53 +1,110 @@
-const user =
-JSON.parse(
-localStorage.getItem("user")
+const user = JSON.parse(
+    localStorage.getItem("user")
 );
 
-if(user){
+if (!user) {
 
-document.getElementById("welcome").innerHTML=
-`Welcome ${user.username} 👋`;
+    location.href = "/login.html";
 
 }
 
-document
-.getElementById("createDatabase")
-.onclick=()=>{
+document.getElementById("welcome").innerHTML =
+`👋 Welcome ${user.username}`;
 
-alert(
-"Create Database Coming Soon"
-);
+loadStats();
+
+async function loadStats(){
+
+    try{
+
+        const db =
+        await API.request("/database/list");
+
+        document.getElementById("dbCount").innerText =
+        db.total || 0;
+
+        document.getElementById("collectionCount").innerText =
+        db.collections || 0;
+
+        document.getElementById("documentCount").innerText =
+        db.documents || 0;
+
+        document.getElementById("apikeyCount").innerText =
+        db.keys || 1;
+
+    }catch(e){
+
+        console.log(e);
+
+    }
+
+}
+
+document.getElementById("createDatabase").onclick =
+async()=>{
+
+    const name =
+    document.getElementById("databaseName").value.trim();
+
+    if(!name) return;
+
+    const data =
+    await API.request(
+
+        "/database/create",
+
+        "POST",
+
+        {
+
+            name
+
+        }
+
+    );
+
+    if(!data.success){
+
+        alert("Gagal");
+
+        return;
+
+    }
+
+    document.getElementById("databaseUrl").value =
+    data.url;
+
+    document.getElementById("apiKey").value =
+    localStorage.getItem("apiKey");
+
+    loadStats();
 
 };
 
-document
-.getElementById("copyUrl")
-.onclick=()=>{
+document.getElementById("copyUrl").onclick=()=>{
 
 navigator.clipboard.writeText(
 
-document
-.getElementById("databaseUrl")
-.value
+document.getElementById("databaseUrl").value
 
 );
 
-alert("URL Copied");
-
 };
 
-document
-.getElementById("copyKey")
-.onclick=()=>{
+document.getElementById("copyKey").onclick=()=>{
 
 navigator.clipboard.writeText(
 
-document
-.getElementById("apiKey")
-.value
+document.getElementById("apiKey").value
 
 );
 
-alert("API KEY Copied");
-
 };
+
+function logout(){
+
+localStorage.clear();
+
+location.href="/login.html";
+
+}
